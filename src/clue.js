814e523880703d045298b392f439289cde976183
Bjork Weenk <bjorkweenk.bw@gmail.com>
2022-04-19 23:06:29 +0200

    create suspect,weapon,rooms

--- src/clue.js
+++ src/clue.js
@@ -1,160 +1,130 @@
-// ITERATION 1
+// suspects [Jacob,Doctor,Victor,Kassandra, Eleanor,Jack]
+const suspectsArray = [
+  {
+      firstName: "Jacob",
+      lastName: "Green",
+      occupation: "Entrepreneur",
+      age: 45,
+      description: "He has a lot of connections",
+      image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
+      color: "green"
+  },
+    {
+    firstName: "Doctor",
+    lastName: "Orchid",
+    occupation: "Scientist",
+    age: 26,
+    description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+    image: "http://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    color: "white",
+  },
+  {
+    firstName: "Victor",
+    lastName: "Plum",
+    occupation: "Designer",
+    age: 22,
+    description: "Billionaire video game designer",
+    image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
+    color: "purple"
+  },
+  {
+    firstName: "Kasandra",
+    lastName: "Scarlet",
+    occupation: "Actor",
+    age: 31,
+    description: "She is an A-list movie star with a dark past",
+    image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
+    color: "red"
+  },
+  {
+    firstName: "Eleanor",
+    lastName: "Peacock",
+    occupation: "Socialité",
+    age: 36,
+    description: "She is from a wealthy family and uses her status and money to earn popularity",
+    image: "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+    color: "blue"
+  },
+  {
+    firstName: "Jack",
+    lastName: "Mustard",
+    occupation: "Retired Football player",
+    age: 62,
+    description: "He is a former football player who tries to get by on his former glory",
+    image: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
+    color: "yellow"
+  }
+]
+ //console.log(suspectsArray)
 
-// Suspects Array
+ // const weaponsArray = [rope,knife,candlestick,dumbbell,poison,axe,bat,trophy,pistol]
+const weaponsArray = [
 
-let mrGreen = {
-firstName: "Jacob",
-lastName: "Green",
-occupation: "Entrepreneur",
-age: 45,
-description: "He has a lot of connections",
-image: "https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg",
-color: "green"
-};
+  { name: "rope",
+    weight: 10 },
 
-let drOrchid = {
-firstName: "Doctor",
-lastName: "Orchid",
-occupation: "Scientist",
-age: 26,
-description: "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
-image:" http://www.radiotimes.com/uploads/images/Original/111967.jpg",
-color: "white",
-};
+  { name: "knife",
+    weight: 8 },
 
-let profPlum = {
-firstName: "Victor",
-lastName: "Plum",
-occupation: "Designer",
-age: 22,
-description: "Billionaire video game designer",
-image: "https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg",
-color: "purple",
-};
+  { name: "candlestick",
+    weight: 2 },
 
-let missScarlet = {
-  name: "Kasandra Scarlet",
-  occupation: "Actor",
-  age: 31,
-  description: "She is an A-list movie star with a dark past",
-  image: "https://www.radiotimes.com/uploads/images/Original/111967.jpg",
-  color: "red",
-};
+  { name: "dumbbell",
+    weight: 30 },
 
-let mrsPeacock = {
-  name: "Eleanor Peacock",
-  occupation: "Socialité",
-  age: 36,
-  description:
-    "She is from a wealthy family and uses her status and money to earn popularity",
-  image: "https://vignette.wikia.nocookie.net/cluedo/images/b/b0/2016C5.jpg/revision/latest?cb=20200927103538",
-  color: "blue",
-};
+  { name: "poison",
+    weight: 2 },
 
-let mrMustard = {
-  name: "Jack Mustard",
-  occupation: "Retired Football player",
-  age: 62,
-  description:
-    "He is a former football player who tries to get by on his former glory",
-  image:
-    "https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg",
-  color: "yellow",
-};
+  { name: "axe",
+    weight: 15 },
 
-const suspectsArray = [mrGreen,drOrchid,profPlum,missScarlet, mrsPeacock, mrMustard];
+  { name: "bat",
+    weight: 13 },
 
-// Weapons array 
+  { name: "trophy",
+    weight: 25},
 
-const weaponsArray = ["rope", "knife","candlestick", "dumbbell", "poison", "axe", "bat","trophy","pistol"];
+  { name: "pistol",
+    weight: 20 }
+  ];
 
-let rope ={
-  name : "rope",
-  weight : 10,
-} 
+ // console.log(weaponsArray)
 
-let knife = { 
-  name: "knife",
-  weight:8 
-}
+  // Rooms Array[Diningroom,conservator,kitchen,study,library,billardroom, lounge, ballroom, hall
+//pa , livingroom, observatory guesthouse, theater, patio]
 
-let candlestick = { 
-  name: "candelstick",
-  weight:2 
-}
+const roomsArray = [
+  {name: "Dining Room" },
 
-let dumbbell = { 
-  name: "dumbbell",
-  weight: 30 
-}
+  { name: "Conservatory"},
 
-let poison = { 
-  name: "poison",
-  weight: 2 
-}
+  { name: "Kitchen" },
 
-let axe = { 
-  name: "axe",
-  weight:15
-}
+  { name: "Study" },
 
-let bat = { 
-  name: "bat",
-  weight:13 
-}
+  {name: "Library" },
 
-let trophy = { 
-  name: "trophy",
-  weight: 25
-}
+  {name: "Billiard Room"},
 
-let pistol = { 
-  name: "pistol",
-  weight: 20 
-}
-// roomsArray
+  { name: "Lounge"},
 
-const roomsArray = ["Dining Room", "Conservatory", "Kitchen", "Study", "Library", "Billiard Room","Lounge", "Ballroom", "Hall", "Spa","LivingRoom","Observatory", "Theater", "GuestHouse", "Patio"];
+  { name: "Ballroom" },
 
-let DiningRoom = {name: "Dining Room"};
-let Conservatory = {name: "Conservatory"};
-let Kitchen = {name: "Kitchen"};
-let Study = {name : "Study"};
-let Library ={name : "Library"};
-let Billiard ={Roomname: "Billiard Room"};
-let Lounge ={name: "Lounge"};
-let Ballroo ={mname: "Ballroom"};
-let Hall ={name: "Hall"};
-let Spa ={ name: "Spa"};
-let LivingRoom ={ name: "Living Room"};
-let Observator ={name:"Observatory"};
-let Theater ={name: "Theater"};
-let Guest ={Housename: "Guest House"};
-let Patio ={name: "Patio"};
+  { name: "Hall"},
 
+  { name: "Spa"},
 
-// ITERATION 2
+  { name: "Living Room"},
 
-function selectRandom() {}
+  {name: "Observatory" },
 
-function pickMystery() {}
+  {name: "Theater" },
 
+  {name: "Guest House"},
 
-// ITERATION 3
+  { name: "Patio"}
+  ];
 
-function revealMystery() {}
+ // console.log(roomsArray)
 
-
-
-// The following is required to make unit tests work.
-/* Environment setup. Do not modify the below code. */
-if (typeof module !== 'undefined') {
-  module.exports = {
-    suspectsArray,
-    roomsArray,
-    weaponsArray,
-    pickMystery,
-    revealMystery,
-    selectRandom
-  };
-}
+  // create a mystery 
